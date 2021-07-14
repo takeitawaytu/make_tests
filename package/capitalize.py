abc966c27cd52a0e307f1d09ab7054b5012463fc
takeitawaytu <takeitawaytu@gmail.com>
2021-07-14 17:06:34 +0300

    update test_capitalize.py

--- package/capitalize.py
+++ package/capitalize.py
@@ -4,4 +4,3 @@ def capitalize(text):
     first_char = text[0].upper()
     rest_substring = text[1:]
     return f'{first_char}{rest_substring}'
-
--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,10 +1,7 @@
 from package.capitalize import capitalize
 
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Wrong working function')
-
-if capitalize('') != '':
-    raise Exception('Wrong working function')
+assert capitalize('hello') == 'Hello'
+assert capitalize('') == ''
 
 print('All tests passed')
